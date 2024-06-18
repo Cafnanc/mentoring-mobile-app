90c73158efea6686ac48772f415061ff7dec4725
Cafnanc <cafnanc@gmail.com>
2024-06-18 09:51:53 +0530

    KB mentoring

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonTabs } from '@ionic/angular';
+import { PAGE_IDS } from 'src/app/core/constants/page.ids';
 
 @Component({
   selector: 'app-tabs',
@@ -9,6 +10,10 @@ import { IonTabs } from '@ionic/angular';
 export class TabsPage {
   private activeTab?: HTMLElement;
   subscription: any;
+  dashboardPageId = PAGE_IDS.dashboard;
+  profilePageId = PAGE_IDS.profile;
+  mentorsPageId = PAGE_IDS.mentorDirectory;
+  homePageId = PAGE_IDS.home;
   constructor() {}
   tabChange(tabsRef: IonTabs) {
     this.activeTab = tabsRef?.outlet?.activatedView?.element;
